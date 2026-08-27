89eee852dd6a7994a7e41362b15f33537d768984
Mouad BANI <mouad-mb@outlook.com>
2026-08-27 15:22:54 +0100

    fix: register dummy connector only in dev environments
    Signed-off-by: Mouad BANI <mouad-mb@outlook.com>

--- services/apps/connectors_worker/src/main.ts
+++ services/apps/connectors_worker/src/main.ts
@@ -1,5 +1,6 @@
 import { Config } from '@crowd/archetype-standard'
 import { Options, ServiceWorker } from '@crowd/archetype-worker'
+import { IS_DEV_ENV } from '@crowd/common'
 import { registerConnector } from '@crowd/connectors'
 import { dummyConnector } from '@crowd/connectors/src/testing/dummyConnector'
 
@@ -31,7 +32,9 @@ export const svc = new ServiceWorker(config, options)
 
 // POC only: dummy connector drives the control-plane end-to-end; real
 // connectors register here starting with GitHub in M4
-registerConnector(dummyConnector)
+if (IS_DEV_ENV) {
+  registerConnector(dummyConnector)
+}
 
 setImmediate(async () => {
   await svc.init()
